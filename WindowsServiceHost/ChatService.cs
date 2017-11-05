a66598a3009159af83b6d57766f1ec402af6622b
Tuandl <anhtuan4597@gmail.com>
2017-11-05 22:47:02 +0700

    completed doc

---
+++ WindowsServiceHost/ChatService.cs
@@ -0,0 +1,53 @@
+﻿using ServerLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceHost
+{
+    public partial class ChatService : ServiceBase
+    {
+        static ServiceHost host = null;
+
+        public ChatService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(Environment.CurrentDirectory + @"\Resources\Avatar\");
+            Console.WriteLine("Avatar location: " + dir.ToString());
+
+            host = new ServiceHost(typeof(ServerLibrary.ChatService));
+            ServerLibrary.ChatService.Init();
+            try
+            {
+                host.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        protected override void OnStop()
+        {
+            if(host != null)
+            {
+                host.Close();
+                host = null;
+            }
+        }
+    }
+}
---
+++ WindowsServiceHost/ChatServiceInstaller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Configuration.Install;
+using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace WindowsServiceHost
+{
+    [RunInstaller(true)]
+    public class ChatServiceInstaller : Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public ChatServiceInstaller()
+        {
+            process = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.NetworkService
+            };
+            service = new ServiceInstaller
+            {
+                ServiceName = "ZolaChatService",
+                DisplayName = "Zola Chat Service",
+                Description = "Host service for Zola Projeck",
+                StartType = ServiceStartMode.Automatic
+            };
+
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+    }
+}
